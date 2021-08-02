3a1c67ba32922bd890bda67008dd69e39a0fd13b
Alexander Kacharava <a.kacharava94@gmail.com>
2021-08-02 02:07:08 +0200

    Initial push reactjs
    Setup of project
    scss compatible
    Component works

---
+++ src/assets/components/MyPictures.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MyPictures() {
+  return (
+    <div className="vg-card">
+      <h2>TITLE</h2>
+      <div className="vg-actions">
+        <button className="vg-btn">Delete</button>
+      </div>
+    </div>
+  );
+}
